dcc8863010a6d9060100b482caf1cf0a9f68227a
treatme030 <treatme8192@gmail.com>
2021-08-27 23:02:36 +0900

    category menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Categories from './Categories';
+import items from './data';
+import Menu from './Menu';
+
+//'all' 추가, 중복 제외한 카테고리
+const allCategories = ['all',...new Set(items.map(item => item.category))]
 
 function App() {
+  const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories] = useState(allCategories)
+
+  const filterItems = (category) => {
+    const newItems = items.filter(item => item.category === category)
+    setMenuItems(newItems)
+  }
   return (
-    <div className="App">
-      <h1>menu</h1>
-    </div>
+    <main>
+      <section className="menu section">
+          <div className="title">
+            <h2>our menu</h2>
+            <div className="underline"></div>
+          </div>
+          <Categories filterItems={filterItems} categories={categories}/>
+          <Menu items={menuItems}/>
+      </section>
+    </main>
   );
 }
 
